ab2a219eb02ec0a79eef1f92c6b1558ce79af244
Jiwei Xia <jiweixia2016@u.northwestern.edu>
2015-10-08 17:26:01 -0500

    Create .gitignore, remove node_module as redundant
    Also connect the database to front

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var bodyParser = require('body-parser');
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'http://paas:456497bb9d985d1ce45f33817d2c6236@fili-us-east-1.searchly.com',
+  log: 'trace'
+});
 
 /* var util = require('util'); //debugging */
  
@@ -30,10 +36,28 @@ app.post('/endpoint', function(req, res) {
 		res.send(500, 'No file found');
 	}
 	*/
-
+	//console.log(req.body.query);
 	var response = {"text":"DEAR OVER IT: Your husband's behavior is passive-aggressive, and I can't help but wonder what he's punishing you for. It's sad that he has such atrocious table manners and such little consideration for your feelings. I 'suggest' you stop trying to serve him a hot meal, let him get his own food from the kitchen and eat it in front of the television when he's hungry, while you eat separately -- preferably out with friends."};
 
-	res.send(response.text);
+	client.search({
+	  index: 'articles',
+	  type: 'article',
+	  body: {
+	    query: {
+	      match: {
+	        'question': req.body.query
+	      }
+	    }
+	  }
+	}).then(function (resp) {
+	    var hits = resp.hits.hits;
+	    res.send(hits[0]._source.question);
+	}, function (err) {
+	    console.trace(err.message);
+	});
+
+
+	
 });
 
 app.listen(3000);
