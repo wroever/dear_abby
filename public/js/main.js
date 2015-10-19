824434dfcbab9f4d6160927d743631d2fc58bcc3
Will Roever <william.roever@gmail.com>
2015-10-18 23:09:48 -0500

    Implemented 'top 3' response functionality, front-end css/js changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
+	function submitQuery() {
+		var data = { query : $("#query").val() };
+		//var data = { "a" : true };
+		//console.log(JSON.stringify(data))
+		$.ajax({
+			type: 'POST',
+			data: JSON.stringify(data),
+	        contentType: 'application/json',
+            url: '/results',						
+            success: function(html) {
+                $("#result").html(html).promise().done(function () {
+                	$(".show-more").click(function () {
+                		$(".result-cont.more").slideDown();
+                		$(this).hide();
+                	});
+                });
+            }
+		});
+	}
+	$("#query").keypress(function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+			submitQuery();
+		}
+	});
 	$("#submit-query").bind("click", function (e) {
 		e.preventDefault();
 		$("#result-text").removeClass("error");
 		if ($("#query").val().length < 1) {
 			$("#result-text").addClass("error").html("Please enter a query.");
 		} else {
-			var data = {};
-
-			data.query = $("#query").val();
-			console.log(JSON.stringify(data))
-			
-			$.ajax({
-				type: 'POST',
-				data: JSON.stringify(data),
-		        contentType: 'application/json',
-                url: '/endpoint',						
-                success: function(data) {
-                    $("#result-text").html(JSON.stringify(data));
-                }
-			});
-			//$("#result-text").load("/endpoint",JSON.stringify(data),function (d) {});
+			submitQuery();
 		}
 	});
 });
--- server.js
+++ server.js
@@ -1,52 +1,70 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var elasticsearch = require('elasticsearch');
-/* var util = require('util'); //debugging http requests */
+var util = require('util'); //debugging http requests
 
 var app = express();
+//var router = express.Router(); //express 4.0 adds router functionality
 
 // set port to env variable if exists or 5000
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 // initialize database connection
 var client = new elasticsearch.Client({
-  host: 'http://paas:456497bb9d985d1ce45f33817d2c6236@fili-us-east-1.searchly.com',
-  log: 'trace'
+	host: 'http://paas:456497bb9d985d1ce45f33817d2c6236@fili-us-east-1.searchly.com',
+	log: 'trace'
 });
 
 // route mgmt
+app.get('/', function(request, response) {
+	response.render('pages/index');
+});
+
+// Need to figure out how to handle ajax request w/ route mgr -> potential security issue
+app.post('/results', function(req, res) {
+    //util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url + '\npath: ' + req.path + '\nbody: ' + req.body) // debugging...
+	client.search({
+	  index: 'articles',
+	  type: 'article',
+	  body: {
+	    query: {
+	      match: {
+	        'submission': req.body.query
+	      }
+	    }
+	  }
+	}).then(function (resp) {
+		// create payload from the search results to send to client
+	    var hits = resp.hits.hits;
+	    var data = { "hits": [] }
+    	var n = 3; // number of results to include in payload
+    	for(i = 0; i < n; i++) {
+    		if (!!hits[i]) {
+    			data.hits.push(hits[i]);
+    		}
+    	}
+
+		res.render('partials/results.ejs', data);
+
+	    //res.status(200).send(result_html);
+	}, function (err) {
+	    console.trace(err.message);
+	    res.status(500).end();
+	});
+
+});
+
+/*
 app.post('/endpoint', function(req, res) {
 	//util.log(util.inspect(req)) // this line helps you inspect the request so you can see whether the data is in the url (GET) or the req body (POST)
     //util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url) // this line logs just the method and url
-    /*
-    if(req.body) {
-
-	    var python = require('child_process').spawn(
-		    'python',
-		    // second argument is array of parameters, e.g.:
-		    ["./dearabbey_spine.py", JSON.stringify(req.body)]
-	    );
-	    var output = "";
-	    python.stdout.on('data', function(){ output += data });
-	    python.on('close', function(code){ 
-	    	if (code !== 0) {  return res.send(500, code); }
-	    	return res.send(200, output)
-	    });
-	} else {
-		res.send(500, 'No file found');
-	}
-	*/
 
 	client.search({
 	  index: 'articles',
@@ -70,14 +88,13 @@ app.post('/endpoint', function(req, res) {
 	    		}
 	    	}
 	    }
-	    res.send(data);
+	    res.send(200, data);
 	}, function (err) {
 		console.log('Elasticsearch error:\n');
 	    console.trace(err.message);
 	});
-
-	
 });
+*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
