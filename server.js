f443214540eaea3d72496d0d6644c85816d364c7
Will Roever <william.roever@gmail.com>
2015-11-30 18:11:53 -0600

    Upped min_score threshold

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/results', function(req, res) {
 	  index: 'articles',
 	  type: atype,
 	  body: {
-	  	min_score: 0.5,
+	  	min_score: 0.7,
 	    query: {
 	      bool: {
 	      	should: [
